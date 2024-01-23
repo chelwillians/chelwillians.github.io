5ba84fcd8fc1f07b3358cf20bea58e156b5d7b60
chelwillians <michel_wrq@hotmail.com>
2024-01-22 21:32:58 -0300

    feat: atualizar algumas informaçÕes

--- resources/js/3-main.js
+++ resources/js/3-main.js
@@ -1,37 +1,37 @@
 //Mostrando o icone de menu após o banner
-$(window).scroll(function(){
+$(window).scroll(function () {
 	var scroll = $(window).scrollTop();
 
-	if(scroll >= 680){
+	if (scroll >= 680) {
 		$('#mobIcon').addClass('ativo');
-	} else{
+	} else {
 		$('#mobIcon').removeClass('ativo');
 	}
 });
 
 //Icone mobile
 
-function clickIcon(){
-		var a = $('#mobIcon').attr("data-ativo");
+function clickIcon() {
+	var a = $('#mobIcon').attr("data-ativo");
 
-		if (a == 0) {
-			$('#mobIcon').attr("data-ativo", "1");
-			$('#mobIcon span:nth-child(2)').addClass('spanAtivo');
-			$('#mobIcon span:nth-child(3)').addClass('spanAtivoN');
-			$('#mobIcon span:nth-child(1), #mobIcon span:nth-child(4)').addClass('spanAtivo');
+	if (a == 0) {
+		$('#mobIcon').attr("data-ativo", "1");
+		$('#mobIcon span:nth-child(2)').addClass('spanAtivo');
+		$('#mobIcon span:nth-child(3)').addClass('spanAtivoN');
+		$('#mobIcon span:nth-child(1), #mobIcon span:nth-child(4)').addClass('spanAtivo');
 
-			$('.contMenuMob').fadeIn();
-		} else {
-			$('#mobIcon').attr("data-ativo", "0");
-			$('#mobIcon span:nth-child(2)').removeClass('spanAtivo');
-			$('#mobIcon span:nth-child(3)').removeClass('spanAtivoN');
-			$('#mobIcon span:nth-child(1), #mobIcon span:nth-child(4)').removeClass('spanAtivo');
+		$('.contMenuMob').fadeIn();
+	} else {
+		$('#mobIcon').attr("data-ativo", "0");
+		$('#mobIcon span:nth-child(2)').removeClass('spanAtivo');
+		$('#mobIcon span:nth-child(3)').removeClass('spanAtivoN');
+		$('#mobIcon span:nth-child(1), #mobIcon span:nth-child(4)').removeClass('spanAtivo');
 
-			$('.contMenuMob').fadeOut();
-		}
+		$('.contMenuMob').fadeOut();
 	}
+}
 
-$('#mobIcon').click(function(){
+$('#mobIcon').click(function () {
 
 	clickIcon();
 
@@ -40,23 +40,23 @@ $('#mobIcon').click(function(){
 
 
 //Link ancora 
-$('a[href^="#"]').bind('click.smoothscroll, click touchend',function (e) {
+$('a[href^="#"]').bind('click.smoothscroll, click touchend', function (e) {
 	e.preventDefault();
-	
+
 	var target = this.hash,
-	$target = $(target);
+		$target = $(target);
 
 	$('html, body').stop().animate({
-		'scrollTop': $target.offset().top-80
+		'scrollTop': $target.offset().top - 80
 	}, 800, 'swing', function () {
-		
+
 	});
 });
 
 //Efeito juntando texto banner
-$(window).on('load', function(){
-	$('.banner .infos h1 span').addClass('active');
-	setTimeout(function(){ 
-		$('.banner .infos h3').addClass('active');
+$(window).on('load', function () {
+	$('.banner .infos .banner__title span').addClass('active');
+	setTimeout(function () {
+		$('.banner .infos .banner__subtitle').addClass('active');
 	}, 500);
 });
